d87a4ba67a97dc9b6abf1af56606c45555c3ab1c
Tyler Goeringer <tyler.goeringer@gmail.com>
2011-02-06 17:21:43 -0500

    Added shape.cpp.

---
+++ src/shape.cpp
@@ -0,0 +1,11 @@
+class Shape {
+    protected:
+        float index_of_refraction;
+        float index_of_reflection;
+    public:
+        float getIndexOfRefraction()
+            { return index_of_refraction; }
+        float getIndexOfReflection()
+            { return index_of_reflection; }
+        
+}
