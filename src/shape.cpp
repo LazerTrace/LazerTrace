384ace0ff669e2454ecb04bac95fc51a015a55c3
Josh Lee <jleedev@gmail.com>
2011-02-20 16:40:16 -0500

    Moved shape's javadoc into their place

--- src/shape.cpp
+++ src/shape.cpp
@@ -1,25 +1,11 @@
-/**
- * Provides an implementation for all objects present in the scene.
- */
-
 #include <utility>
 #include "shape.hpp"
 
-/**
- * @return Returns the index of refraction.
- */
-float Shape::getIndexOfRefraction()
-{ return index_of_refraction; }
+float Shape::getIndexOfRefraction() const {
+    return index_of_refraction;
+}
 
 
-/**
- * Creates a sphere primitive with the given parameters.
- *
- * @param center The origin of the sphere.
- * @param radius The radius of the sphere.
- * @param index_of_refraction Index of refraction, specific to the
- * material of the sphere.
- */
 Shere::Sphere(Point center, float radius, float index_of_refraction) {
     this.center = center;
     this.radius = radius;
@@ -30,28 +16,13 @@ Sphere::pair<Point, float> getIntersection(Ray ray) {
     return make_pair(center, 0.0); // Placeholder
 }
 
-
-/**
- * Represents an infinite plane.
- */
-
-/**
- * Creates a plane primitive with the given parameters.
- *
- * @param center The origin of the plane (any point on the plane.
- * @param normal The vector normal to the plane.
- * @param index_of_refraction Index of refraction, specific to the
- * material of the plane.
- */
 Plane::Plane(Point center, Vector normal, float index_of_refraction) {
     this.center = center;
     this.normal = normal.normalize();
     this.index_of_refraction = index_of_refraction;
-
-
 }
 
-Plane::pair<Point, float> getIntersection(Ray ray) {
+Plane::pair<Point, float> getIntersection(Ray ray) const {
 
     //return make_pair(center, 0.0); // Placeholder
 }
