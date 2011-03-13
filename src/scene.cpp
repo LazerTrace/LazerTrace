52655fe7be52f3fb56f9c4ae394b913c6937d499
Tyler Goeringer <tyler.goeringer@gmail.com>
2011-03-13 16:22:11 -0400

    Fixed error with incorrect calls to Color().

--- src/scene.cpp
+++ src/scene.cpp
@@ -13,9 +13,9 @@ using std::vector;
  * scene for rendering.
  */
 Scene::Scene() {
-    shapes.push_back(new Sphere(Point(0,0,0), 7.0, Color(255, 0, 0), 0.0));
-    shapes.push_back(new Sphere(Point(10,10,10), 3.0, Color(0, 255, 0), 0.7));
-    shapes.push_back(new Plane(Point(10,0,0), Vector(1,0,0), Color(0, 0, 255), 0.5));
+    shapes.push_back(new Sphere(Point(0,0,0), 7.0, Color(0xff, 0x00, 0x00, 0x00), 0.0));
+    shapes.push_back(new Sphere(Point(10,10,10), 3.0, Color(0, 0xff, 0, 0x00), 0.7));
+    shapes.push_back(new Plane(Point(10,0,0), Vector(1,0,0), Color(0x00, 0x00, 0xff, 0x00), 0.5));
     // Yet to add default lights.
 }
 
