aebb38e6f6794c74fe19c4e6fd2f352db0c0cace
Josh Lee <jleedev@gmail.com>
2011-03-20 15:11:15 -0400

    Completed the default scene object

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
 #include "light.hpp"
 
 int main(void) {
-    Color c(0x00,0x00,0x00);
-    Vector v(0,0,0);
-    Point p(0,0,0);
-    LightSource* ls = new LightSource(c,0);
-    PointLight* pl = new PointLight(c,0,p);
-    DirectionalLight* dl = new DirectionalLight(c,0,v);
-    SpotLight* sl = new SpotLight(c, 0, p, v, 0);
-    
-    
-    delete ls;
-    delete pl;
-    delete dl;
-    delete sl;
+    Scene default_scene;
+
     return 0;
 }
--- src/scene.cpp
+++ src/scene.cpp
@@ -13,10 +13,21 @@ using std::vector;
  * scene for rendering.
  */
 Scene::Scene() {
-    shapes.push_back(new Sphere(Point(0,0,0), 7.0, Color(0xff, 0x00, 0x00), 0.0));
-    shapes.push_back(new Sphere(Point(10,10,10), 3.0, Color(0, 0xff, 0), 0.7));
-    shapes.push_back(new Plane(Point(10,0,0), Vector(1,0,0), Color(0x00, 0x00, 0xff), 0.5));
-    // Yet to add default lights.
+    Color c_zero(0x00,0x00,0x00);
+    Vector v_zero(0,0,0);
+    Point p_zero(0,0,0);
+
+    shapes.push_back(new Sphere(Point(0,0,0), 7.0,
+                Color(0xff, 0x00, 0x00), 0.0));
+    shapes.push_back(new Sphere(Point(10,10,10), 3.0,
+                Color(0, 0xff, 0), 0.7));
+    shapes.push_back(new Plane(Point(10,0,0), Vector(1,0,0),
+                Color(0x00, 0x00, 0xff), 0.5));
+
+    lights.push_back(new LightSource(c,0));
+    lights.push_back(new PointLight(c,0,p));
+    lights.push_back(new DirectionalLight(c,0,v));
+    lights.push_back(new SpotLight(c, 0, p, v, 0));
 }
 
 Scene::Scene(std::string fileName) {
