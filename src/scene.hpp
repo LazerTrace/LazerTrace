9b588bb228c8a9d3afc37c2eae5ecb504b3a8d07
Tyler Goeringer <tyler.goeringer@gmail.com>
2011-03-20 17:01:26 -0400

    Added Scene::scene().

--- src/scene.hpp
+++ src/scene.hpp
@@ -17,8 +17,29 @@ protected:
     Camera camera;
 
 public:
+    /**
+     * Default constructor for objects of class Scene().
+     * Creates a default scene layout for testing purposes,
+     * with a minimal number of objects and lights.
+     */
     Scene();
+    
+    /**
+     * Constructor for objects of class Scene().
+     * Loads a scene from a file using JSON and OpenCTM file formats.
+     */
     Scene(std::string fileName);
+    
+    /**
+     * Given an object and the ray representing the intersection point,
+     * returns the expected color given the local illumination calculation.
+     *
+     * @param obj The object which a ray intersected.
+     * @param hit The ray representing the normal vector and point of intersection.
+     * @return The color at the given point.
+     */
+    Color shade(Shape obj, Ray hit);
+    
     ~Scene();
 
     /**
