9ef77fbb2f9a93e6ac6e615d64f31cfbafca82c5
Zev <Zev.Goldstein@gmail.com>
2011-04-02 04:14:35 -0400

    reverted the sign change on cosine in the shade function because the real problem was actually the sphere intersection.  Also modified the default scene

--- src/scene.cpp
+++ src/scene.cpp
@@ -19,15 +19,30 @@ using std::auto_ptr;
 Scene::Scene() : shapes(vector<Shape*>()),
                  lights(vector<LightSource*>()),
                  ambient(Color(0.25, 0.25, 0.25)),
-                 camera(Camera()) {
-    shapes.push_back(new Plane(Point(0, -3, 0), Vector(0, 1, 0),
-                Color(0, 1, 0), 1, 0.5, 0.5, 0.5));
-    shapes.push_back(new Sphere(Point(1, -1, 5), 1,
+                 camera(Camera())
+{
+    //floor
+    shapes.push_back(new Plane(Point(0, -3, 0), Vector(0, 1, 0), Color(0, 1, 0), 1, 0.5, 0.5, 0.5));
+    
+    //back wall
+    //shapes.push_back(new Plane(Point(0, 0, 20), Vector(0, 0, -1), Color(1, 1, 0), 1, 0.5, 0.5, 0.5));
+    
+    //left side wall
+    //shapes.push_back(new Plane(Point(-3, 0, 0), Vector(1, 0, 0), Color(0, 1, 1), 1, 0.5, 0.5, 0.5));
+    
+    //right sphere (red)
+    shapes.push_back(new Sphere(Point(3, -1, 5), 2,
                 Color(1, 0, 0), 1, 0.9, 0.5, 0.5));
-    shapes.push_back(new Sphere(Point(-1, -1, 5), 1,
+                
+    //left sphere (blue)
+    shapes.push_back(new Sphere(Point(-1, 2, 5), 1,
                 Color(0, 0, 1), 1, 0.3, 0.5, 0.5));
 
-    lights.push_back(new PointLight(Color(1, 1, 1), Point(0, 5, 5)));
+    //light source 1
+    //lights.push_back(new PointLight(Color(1, 1, 1), Point(0, .001, 2)));
+    
+    //light source 2
+    lights.push_back(new PointLight(Color(1, 1, 1), Point(0, 5, 0)));
 }
 
 Scene::Scene(std::string fileName): shapes(vector<Shape*>()),
@@ -66,22 +81,30 @@ Color Scene::raytrace(const Ray& camera_ray) const {
 }
 
 Color Scene::shade(const Shape *obj, Ray hit) const{
+    /*
+    if(hit.getDir().i==0 && hit.getDir().j==1 && hit.getDir().k==0){
+        std::cout << hit.getOrigin().x << "\n";
+        hit.getOrigin().x << ", " << hit.getOrigin().y << ", " << hit.getOrigin() << "\n";
+        //return obj->getColor();
+        
+    }
+    */
     for(vector<LightSource*>::const_iterator it = lights.begin(); it != lights.end(); it++) {
         LightSource *light = *it;
         Ray shadow = Ray::makeRay(hit.getOrigin(), light->getPoint());
         Color result = Color(ambient) * obj->getAmbientCoefficient();
         bool collision = false;
-
         // Detect collisions, for now do nothing if there is a collision
         for(vector<Shape*>::const_iterator sh = shapes.begin(); sh != shapes.end(); sh++) {
             Shape *shape = *sh;
-            auto_ptr<Ray> hit(shape->getIntersection(shadow));
-            if (hit.get() != NULL && hit->getOrigin() != shadow.getOrigin()) {
-                collision = true;
-                break;
+            if (shape != obj){
+                auto_ptr<Ray> hit(shape->getIntersection(shadow));
+                if (hit.get() != NULL && hit->getOrigin() != shadow.getOrigin()) {
+                    collision = true;
+                    break;
+                }
             }
         }
-
         // Calculate local illumination
         if(!collision) {
             // Normalize the direction vectors before calculations
@@ -89,18 +112,32 @@ Color Scene::shade(const Shape *obj, Ray hit) const{
             hit.normalize();
 
             float cos_theta = (shadow.getDir()).dotProduct(hit.getDir());
-
+            /*
+            if(!(hit.getDir().i==0 && hit.getDir().j==1 && hit.getDir().k==0)){
+                //zprint:Vector(hit.getDir());
+                std::cout<< hit.getDir().i << ",";
+                std::cout<< hit.getDir().j << ",";
+                std::cout<< hit.getDir().k << "\n";
+            }
+            */
             if(cos_theta < 0)
-                cos_theta = -cos_theta;
-            else
                 cos_theta = 0;
-
+            /*
+            if(hit.getDir().i==0 && hit.getDir().j==1 && hit.getDir().k==0){
+                std::cout<<cos_theta;
+            }
+            */
             Color diffuse = cos_theta * obj->getDiffuseCoefficient()
                 * light->getColor();
             result = result + diffuse;
-        }
-        
-        
+        }/*
+        if(hit.getDir().i==0 && hit.getDir().j==1 && hit.getDir().k==0){
+            
+            std::cout<< result.red << ",";
+            std::cout<< result.blue << ",";
+            std::cout<< result.green << "\n";
+            
+        }*/
         result = result * obj->getColor();
         return result;
     }
@@ -117,3 +154,10 @@ Scene::~Scene() {
 Ray Scene::get_camera_ray(float x, float y) const {
     return camera.get_ray(x, -y);
 }
+class print{
+    static void Vector(Vector v){
+        std::cout<< v.i << ",";
+        std::cout<< v.j << ",";
+        std::cout<< v.k << "\n";
+    }
+};
