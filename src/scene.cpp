6ee91ac41f06839d48d85f10d42b12b1d1a4e499
Josh Lee <jleedev@gmail.com>
2011-04-10 15:55:25 -0400

    Tilted the shiny new camera

--- src/scene.cpp
+++ src/scene.cpp
@@ -24,8 +24,8 @@ using std::auto_ptr;
 Scene::Scene(int width, int height) : shapes(vector<Shape*>()),
                  lights(vector<LightSource*>()),
                  ambient(Color(0.25, 0.25, 0.25)),
-                 camera(Camera::lookAt(Point(0,2,-5), Vector(0, 1, 0),
-                             Point(0, 0, 0), degrees(60),
+                 camera(Camera::lookAt(Point(0,2,-5), Vector(0.1, 1, 0),
+                             Point(0, 1, 0), degrees(60),
                              width, height))
 {
     // floor
